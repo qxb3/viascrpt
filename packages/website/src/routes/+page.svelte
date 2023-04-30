a3e43386f782a5c21aa609e6919c90223f75a026
qxb3 <qxbthree@gmail.com>
2023-05-01 02:22:20 +0800

    feat(website): added content on root route

--- packages/website/src/routes/+page.svelte
+++ packages/website/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="container p-4">
+  <div class="text-center mt-16">
+    <h1 class="font-bold !text-5xl">
+      <span class="text-blue-200">Via</span>
+      <span class="text-lime-300">Scrpt</span>
+    </h1>
+
+    <p class="mt-3 !text-xl underline underline-offset-3">Find and Share via scripts to others!</p>
+  </div>
+
+  <div class="text-center mt-16">
+    <button class="btn btn-lg variant-ringed-primary">
+      <span>Find Scripts</span>
+      <span>
+        <li class="fa fa-search"></li>
+      </span>
+    </button>
+  </div>
+</div>
