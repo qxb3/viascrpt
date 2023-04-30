13c9c1092910a65ca5c981118f90787440415504
qxb3 <qxbthree@gmail.com>
2023-05-01 02:21:58 +0800

    feat(website): added Menu for nav burger, and shared vars

---
+++ packages/website/src/lib/modals/Menu.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { ListBox, ListBoxItem, modalStore } from '@skeletonlabs/skeleton'
+  import { links } from '$lib/vars.js'
+</script>
+
+<div class="w-[97%] bg-surface-500 p-4 rounded-md border-[0.1px] border-surface-400">
+  <div class="flex place-content-between">
+    <ListBox
+      rounded="rouned-none"
+      active="bg-surface-500"
+      hover="bg-surface-500"
+      class="grow"
+    >
+      {#each links as link}
+        <ListBoxItem class="hover:underline">
+          <span>
+            <li class="fa {link.icon}"></li>
+          </span>
+          {link.title}
+        </ListBoxItem>
+      {/each}
+    </ListBox>
+
+    <button on:click={() => modalStore.close()} class="btn h-fit">
+      <i class="fa fa-times fa-lg"></i>
+    </button>
+  </div>
+
+  <div class="my-3 h-[0.1px] bg-surface-400"></div>
+
+  <div class="mt-7">
+    <button class="btn variant-filled-primary">Sign Up</button>
+  </div>
+</div>
---
+++ packages/website/src/lib/vars.js
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    title: 'Popular Scripts',
+    path: '/scripts?sort=top',
+    icon: 'fa-star'
+  },
+  {
+    title: 'All Scripts',
+    path: '/scripts',
+    icon: 'fa-list'
+  },
+  {
+    title: 'About Us',
+    path: '/about-us',
+    icon: 'fa-info-circle'
+  },
+  {
+    title: 'FaQ',
+    path: '/faq',
+    icon: 'fa-question-circle'
+  },
+]
+
+export default {
+  links
+}
