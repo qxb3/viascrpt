4de2b7c606e5e7cfd8b71d27f5e6522eaf959384
qxb3 <qxbthree@gmail.com>
2023-05-01 21:46:37 +0800

    feat(website): script page halfway done

--- packages/website/src/routes/scripts/[name]/+page.svelte
+++ packages/website/src/routes/scripts/[name]/+page.svelte
@@ -1,7 +1,16 @@
 <script>
   import CodeBlock from '$lib/components/CodeBlock.svelte'
+  import { clipboard, toastStore } from '@skeletonlabs/skeleton'
 
   export let data
+
+  function copy(message) {
+    toastStore.clear()
+    toastStore.trigger({
+      message,
+      background: 'variant-filled-success'
+    })
+  }
 </script>
 
 <svelte:head>
@@ -10,7 +19,7 @@
 
 <div class="container p-4">
   <div class="card p-4">
-    <div>
+    <div class="pb-4">
       <div class="flex items-center gap-4">
         <h1 class="font-extrabold">
           <span class="text-primary-500">{data.name}</span>
@@ -20,7 +29,9 @@
       <p>{data.description}</p>
     </div>
 
-    <div class="mt-4 py-8">
+    <div class="card h-[0.1px]"></div>
+
+    <div class="py-4 pb-8">
       <h2 class="font-bold mb-4">Code:</h2>
       <CodeBlock
         language='js'
@@ -31,17 +42,26 @@
 
     <div class="card h-[0.1px]"></div>
 
-    <div class="pt-8">
-      <button class="btn btn-sm variant-ghost-primary">
-        Copy Script
+    <div class="flex flex-wrap items-center gap-3 pt-8">
+      <button on:click={() => copy('Copied Script!')} use:clipboard={data.code} class="btn btn-sm variant-filled-tertiary">
+        <span>
+          <li class="fa fa-code"></li>
+        </span>
+        <span>Copy Script</span>
       </button>
 
-      <button class="btn btn-sm variant-ghost-primary">
-        Copy Url
+      <button on:click={() => copy('Copied URL!')} class="btn btn-sm variant-filled-tertiary">
+        <span>
+          <li class="fa fa-link"></li>
+        </span>
+        <span>Copy URL</span>
       </button>
 
-      <button class="btn btn-sm variant-ghost-primary">
-        Share Script
+      <button class="btn btn-sm variant-filled-tertiary">
+        <span>
+          <li class="fa fa-share"></li>
+        </span>
+        <span>Share</span>
       </button>
     </div>
   </div>
