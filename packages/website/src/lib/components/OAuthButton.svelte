8ff437b090414f7ac66121b5707a479ece27ce8b
qxb3 <qxbthree@gmail.com>
2023-05-01 10:00:22 +0800

    feat(website): added OAuthButton lib component

---
+++ packages/website/src/lib/components/OAuthButton.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let title
+  export let icon
+  export let background
+</script>
+
+<div class="flex items-center gap-3 rounded-sm p-4 border-[0.1px] border-surface-400 {background}">
+  <span>
+    <i class="{icon} fa-lg"></i>
+  </span>
+
+  <div>
+    <p class="!text-lg">{title}</p>
+  </div>
+</div>
