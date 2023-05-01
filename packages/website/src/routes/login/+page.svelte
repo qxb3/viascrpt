c66813eb5a913a16fa34341908775dbaab36acf4
qxb3 <qxbthree@gmail.com>
2023-05-01 09:59:36 +0800

    feat(website): added login and signup routes

---
+++ packages/website/src/routes/login/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  import OAuthButton from '$lib/components/OAuthButton.svelte'
+
+  const oAuthButtons = [
+    {
+      title: 'Login with Google',
+      icon: 'fab fa-google',
+      background: 'bg-google'
+    },
+    {
+      title: 'Login with Github',
+      icon: 'fab fa-github',
+      background: 'bg-github'
+    }
+  ]
+</script>
+
+<div class="container p-4">
+  <div class="mt-8 text-center">
+    <h1>Sign In</h1>
+  </div>
+
+  <div class="mt-12 max-w-lg mx-auto space-y-4">
+    {#each oAuthButtons as oAuthButton}
+      <OAuthButton {...oAuthButton} />
+    {/each}
+  </div>
+
+  <div class="text-center mt-8">
+    <p>
+      Dont have an account?
+      <a href="/signup">Sign Up!</a>
+    </p>
+  </div>
+</div>
