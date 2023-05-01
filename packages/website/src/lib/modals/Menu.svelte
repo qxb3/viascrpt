ab36a1e8cbdace922a77ccc043c3b6ca634900bb
qxb3 <qxbthree@gmail.com>
2023-05-01 19:13:32 +0800

    feat(website): change signin text color in Menu lib component for better contrast

--- packages/website/src/lib/modals/Menu.svelte
+++ packages/website/src/lib/modals/Menu.svelte
@@ -29,6 +29,6 @@
   <div class="my-3 h-[0.1px] bg-surface-400"></div>
 
   <div class="mt-7">
-    <a href="/login" class="btn variant-filled-primary">Sign In</a>
+    <a href="/login" class="btn variant-filled-primary !text-surface-900">Sign In</a>
   </div>
 </div>
