70d649b1891faf11485e7bf1bd10503427b2891d
qxb3 <qxbthree@gmail.com>
2023-05-01 10:04:40 +0800

    ref(website): removed some nav links

--- packages/website/src/lib/vars.js
+++ packages/website/src/lib/vars.js
@@ -8,17 +8,7 @@ export const links = [
     title: 'All Scripts',
     path: '/scripts',
     icon: 'fa-list'
-  },
-  {
-    title: 'About Us',
-    path: '/about-us',
-    icon: 'fa-info-circle'
-  },
-  {
-    title: 'FaQ',
-    path: '/faq',
-    icon: 'fa-question-circle'
-  },
+  }
 ]
 
 export default {
